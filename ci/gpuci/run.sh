a87b28807f05552f7cf9556d190d98fffebb9dcd
Mike Wendt <mike.wendt@me.com>
2020-05-05 13:01:28 -0400

    ENH Change default channel to gpuci

--- ci/gpuci/run.sh
+++ ci/gpuci/run.sh
@@ -9,4 +9,4 @@ export PATH="/opt/conda/bin:$PATH"
 source activate base
 conda install -k conda-build conda-verify anaconda-client
 conda build conda/recipe
-anaconda -t ${MY_UPLOAD_KEY} upload -u ${CONDA_USERNAME:-rapidsai-nightly} --label main --skip-existing /opt/conda/conda-bld/linux-64/gpuci-tools*.tar.bz2
+anaconda -t ${MY_UPLOAD_KEY} upload -u ${CONDA_USERNAME:-gpuci} --label main --skip-existing /opt/conda/conda-bld/linux-64/gpuci-tools*.tar.bz2
