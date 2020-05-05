af7ed3717b952537da0d9851037107f57094500d
Mike Wendt <mike.wendt@me.com>
2020-05-05 12:53:18 -0400

    FIX Add missing anaconda-client for upload

--- ci/gpuci/run.sh
+++ ci/gpuci/run.sh
@@ -7,6 +7,6 @@ set -ex
 
 export PATH="/opt/conda/bin:$PATH"
 source activate base
-conda install -k conda-build conda-verify
+conda install -k conda-build conda-verify anaconda-client
 conda build conda/recipe
 anaconda -t ${MY_UPLOAD_KEY} upload -u ${CONDA_USERNAME:-rapidsai-nightly} --label main --skip-existing /opt/conda/conda-bld/linux-64/gpuci-tools*.tar.bz2
